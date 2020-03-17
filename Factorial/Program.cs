637319cc668672e385f950967b4a67e75adfac0a
kclaka <kennyigbechi@gmail.com>
2020-03-17 10:16:32 -0700

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Factorial
+{
+    class CalculateFactor
+    {
+        public static int getFactorial(int x)
+        {
+            if (x == 0)
+                return 1;
+
+            return x * getFactorial(x - 1);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateFactor cal = new CalculateFactor();
+            Console.WriteLine(cal.getFactorial(25));
+        }
+    }
+}
